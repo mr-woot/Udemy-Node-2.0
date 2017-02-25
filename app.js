6799ab48baf969420f84ecb4d35f3f942bf6ab75
Tushar Mudgal <mr-woot@Tushar.local>
2017-02-26 01:19:06 +0530

    feat: response object

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&',
     json: true
 }, (err, res, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(JSON.stringify(res, undefined, 2));
 });
