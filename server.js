ff94c4c87528b5353b39aa1ba35e3c0a62599263
Tushar Mudgal <mr-woot@Tushar.local>
2017-02-25 15:19:07 +0530

    lodash added

--- server.js
+++ server.js
@@ -1,35 +1,101 @@
-var express = require("express");
-var morgan = require("morgan");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+console.log("Starting server.js");
 
-var app = express();
+const fs = require('fs');
+const os = require('os');
 
-// Connect to MongoDb database
-mongoose.connect("mongodb://root:abc123@ds157479.mlab.com:57479/ecommerce", (err) => {
+var user = os.userInfo();
+
+fs.appendFile('file.txt', `Hola ${user.username}..! `, (err) => {
     if (err) {
         console.log(err);
-    } else {
-        console.log("Connected to the database");
     }
-})
-
-// Middlewares
-app.use(morgan("dev"));
-
-
-// Routes
-app.get("/", (req, res) => {
-    res.sendStatus(200);
 });
 
-app.get("/name", (req, res) => {
-    var name = "Tushar";
-    res.json(`My name is ${name}`);
-});
 
-// Server
-app.listen(3000, function(err) {
-    if (err) throw err;
-    console.log("Listening at port 3000");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var express = require("express");
+// var morgan = require("morgan");
+// var mongoose = require("mongoose");
+// var bodyParser = require("body-parser");
+
+// var User = require("./models/user");
+
+// var app = express();
+
+// // Connect to MongoDb database
+// mongoose.connect("mongodb://root:abc123@ds157479.mlab.com:57479/ecommerce", (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Connected to the database");
+//     }
+// })
+
+// // Middlewares
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.post("/createUser", (req, res) => {
+//     var user = new User();
+//     user.profile.name = req.body.name;
+//     user.password = req.body.password;
+//     user.email = req.body.email;
+//     user.save((err) => {
+//         if (err) next(err);
+//         res.json("Successfully posted user details... :P");
+//     });
+// });
+
+// // Routes
+// app.get("/", (req, res) => {
+//     res.sendStatus(200);
+// });
+
+// app.get("/name", (req, res) => {
+//     var name = "Tushar";
+//     res.json(`My name is ${name}`);
+// });
+
+// // Server
+// app.listen(3000, function (err) {
+//     if (err) throw err;
+//     console.log("Listening at port 3000");
+// });
