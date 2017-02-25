aec64f78a2c596a533c7fd1d2f6e800180a646fd
Tushar Mudgal <mr-woot@Tushar.local>
2017-02-25 15:28:23 +0530

    uninstall express

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ console.log("Starting server.js");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes');
 
 var user = os.userInfo();
 
