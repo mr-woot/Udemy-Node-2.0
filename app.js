a88d68b8b108cc3fe575bab2f71d13a9926d1910
Tushar Mudgal <mr-woot@Tushar.local>
2017-02-26 01:45:39 +0530

    feat: lat & lng

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&',
     json: true
 }, (err, res, body) => {
-    console.log(JSON.stringify(res, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
